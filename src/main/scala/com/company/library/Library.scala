1b5924e6b3dbbbb904a78cc837932f0adcb5f9c0
elly <elly.chan.mda@gmail.com>
2019-04-10 22:54:01 +0100

    add #borrowBook with test, book with default #isBookBorrowed status => false

--- src/main/scala/com/company/library/Library.scala
+++ src/main/scala/com/company/library/Library.scala
@@ -1,5 +1,6 @@
 package com.company.library
 
+import scala.collection.mutable
 import scala.collection.immutable
 import scala.collection.mutable.ListBuffer
 
@@ -8,14 +9,18 @@ class Library {
 
   val authorMap:immutable.Map[String, List[Book]] = Books.all.groupBy(book => book.author)
 
+  val isBookBorrowed:mutable.HashMap[Book, Boolean] = mutable.HashMap(
+    Books.all.map(x => (x -> false)): _*
+  )
+
   def findBookByTitle(title: String): List[Book] = {
     Books.all.filter(_.title.contains(title))
   }
 
-  def findBookByAuthor(author: String) = {
+  def findBookByAuthor(author: String): List[Book] = {
     val matchBooks = new ListBuffer[Book]()
-    val matchAuthor = this.authorMap.keys.filter(_.contains(author))
-    matchAuthor.foreach(author => this.authorMap.get(author) match {
+    val matchAuthors = this.authorMap.keys.filter(_.contains(author))
+    matchAuthors.foreach(author => this.authorMap.get(author) match {
       case Some(value) => matchBooks ++= value
     })
     matchBooks.toList
@@ -23,6 +28,20 @@ class Library {
 
   def findBookByIsbn(isbn: String): Book = {
     Books.all.filter(_.ISBN == isbn).head
+  }
+
+  def borrowBook(book: Book): Boolean = {
+    this.isBookBorrowed.get(book) match {
+      case Some(borrowed) => {
+        if (borrowed) {
+          throw new InternalError("Already borrowed this book!!")
+        } else {
+          this.isBookBorrowed(book) = true
+          this.isBookBorrowed(book)
+        }
+      }
+      case None => throw new NoSuchElementException("Book doesn't exist!")
+    }
   }
 
 }
--- src/test/scala/com/company/library/LibrarySpec.scala
+++ src/test/scala/com/company/library/LibrarySpec.scala
@@ -12,12 +12,31 @@ class LibrarySpec extends FunSuite {
 
   test("#findBookByAuthor") {
     val library = new com.company.library.Library()
-    library.findBookByAuthor("Khaled") shouldBe List(Book("Kite Runner,The", "Hosseini, Khaled", "brldgqczq"), Book("Thousand Splendid Suns,A", "Hosseini, Khaled", "itlizwzbz"))
+    library.findBookByAuthor("Khal") shouldBe List(Book("Kite Runner,The", "Hosseini, Khaled", "brldgqczq"), Book("Thousand Splendid Suns,A", "Hosseini, Khaled", "itlizwzbz"))
   }
 
   test("#findBookByIsbn find book by full ISBN") {
     val library = new com.company.library.Library()
     library.findBookByIsbn("pidtkl") shouldBe (Book("Da Vinci Code,The", "Brown, Dan", "pidtkl"))
   }
+
+  test("#isBookBorrowed borrow non-existed book return error") {
+    val library = new com.company.library.Library()
+    val nonExistBook = Book("a book", "an author", "a isbn")
+    an [NoSuchElementException] should be thrownBy(library.isBookBorrowed(nonExistBook))
+  }
+
+  test("#borrowBook can borrow book if book available") {
+    val library = new com.company.library.Library()
+    val book = Book("Da Vinci Code,The", "Brown, Dan", "pidtkl")
+    library.isBookBorrowed(book) shouldBe false
+    library.borrowBook(book)
+    library.isBookBorrowed(book) shouldBe true
+    the [InternalError] thrownBy(library.borrowBook(book)) should have message "Already borrowed this book!!"
+  }
+
+//  test("") {
+//                  ???
+//  }
 
 }
