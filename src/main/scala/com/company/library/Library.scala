0cb17b5af00ca56cdecc1bb537ae5414da9b10a9
elly <elly.chan.mda@gmail.com>
2019-04-10 20:13:36 +0100

    add #findBookByTitle with test

--- src/main/scala/com/company/library/Library.scala
+++ src/main/scala/com/company/library/Library.scala
@@ -2,6 +2,10 @@ package com.company.library
 
 class Library {
 
+  def findBookByTitle(title: String): List[Book] = {
+    Books.all.filter(_.title.contains(title))
+  }
+
   def findBookByIsbn(isbn: String): Book = {
     Books.all.filter(_.ISBN == isbn).head
   }
--- src/test/scala/com/company/library/LibrarySpec.scala
+++ src/test/scala/com/company/library/LibrarySpec.scala
@@ -5,6 +5,11 @@ import org.scalatest.Matchers._
 
 class LibrarySpec extends FunSuite {
 
+  test("#findBookByTitle find book by partial title" ) {
+    val library = new com.company.library.Library()
+    library.findBookByTitle("Life") shouldBe List(Book("Life of Pi", "Martel, Yann", "nggzbsum"), Book("You are What You Eat:The Plan That Will Change Your Life", "McKeith, Gillian","xskevg"))
+  }
+
   test("#findBookByIsbn find book by full ISBN") {
     val library = new com.company.library.Library()
     library.findBookByIsbn("pidtkl") shouldBe (Book("Da Vinci Code,The", "Brown, Dan", "pidtkl"))
